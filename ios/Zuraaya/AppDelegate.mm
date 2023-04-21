d31935ebaab4f4f8036978770cb996a09eac7d6f
Emmanuel Okpunor <e.okpunor@clanelaglp018s-MacBook-Pro.local>
2023-04-21 17:21:00 +0100

    app name

--- ios/Zuraaya/AppDelegate.mm
+++ ios/Zuraaya/AppDelegate.mm
@@ -44,7 +44,7 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 #endif
 
   NSDictionary *initProps = [self prepareInitialProps];
-  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"Wedo", initProps);
+  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"Zuraaya", initProps);
 
   if (@available(iOS 13.0, *)) {
     rootView.backgroundColor = [UIColor systemBackgroundColor];
--- ios/ZuraayaTests/ZuraayaTests.m
+++ ios/ZuraayaTests/ZuraayaTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface WedoTests : XCTestCase
+@interface ZuraayaTests : XCTestCase
 
 @end
 
-@implementation WedoTests
+@implementation ZuraayaTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
