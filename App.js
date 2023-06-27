ca913dc384248bc2a30825f5b5d5e7ee63cc41e7
LemmyOnukwugha <lemmyonukwugha@yahoo.com>
2023-06-27 23:31:44 +0100

    ios splashscreen

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import React, {useEffect} from 'react';
 import Routes from './app/Navigations/Route';
 import {Provider} from 'react-redux';
+import RNBootSplash from 'react-native-bootsplash';
+
 import {store} from './store';
 
-import useNetwork from './hooks/useNetwork';
-
 const App = () => {
+  useEffect(() => {
+    const init = async () => {
+      // …do multiple sync or async tasks
+    };
+
+    init().finally(async () => {
+      await RNBootSplash.hide({fade: true, duration: 500});
+      console.log('Bootsplash has been hidden successfully');
+    });
+  }, []);
   return (
     <>
       <Provider store={store}>
